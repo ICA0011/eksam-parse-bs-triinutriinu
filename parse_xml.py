fec598df9bf794456638f62c0689abc3c5b5806c
triinutriinu <trparn@ttu.ee>
2022-05-26 12:43:31 +0300

    exam

--- parse_xml.py
+++ parse_xml.py
@@ -1,12 +1,23 @@
+from xml.etree.ElementTree import fromstring
+
 from bs4 import BeautifulSoup
 import requests
-import lxml
+from lxml import etree
+
 
 def parse_xml():
   url = "http://upload.itcollege.ee/~aleksei/test.xml"
   xml_content = requests.get(url).content
-  soup = BeautifulSoup(xml_content,'xml')
+  soup = BeautifulSoup(xml_content, 'xml')
   
   # your code here
-  
-  return result
+
+  result = ""
+
+  x = soup.get_text("</data>")
+  y = soup.getText("</data>")
+
+  return x.split()[1].strip()
+
+
+print(parse_xml())
--- parse_xml_test.py
+++ parse_xml_test.py
@@ -1,4 +1,8 @@
 import parse_xml as pxml;
 
+
 def test_parsing():
     assert pxml.parse_xml() == "Successful"
+
+
+print(test_parsing())
